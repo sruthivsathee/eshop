ef0b67ecda9725b92382d29b84080c0fccdbc7e6
Sruthi VS <46927753+sruthivsathee@users.noreply.github.com>
2025-03-28 18:17:19 +0530

    Resolved dependency conflict for react-currency-format

--- src/Components/Subtotal.js
+++ src/Components/Subtotal.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "./Subtotal.css";
-import CurrencyFormat from "react-currency-format";
+import CurrencyInput from "react-currency-input-field";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
-      <CurrencyFormat
-        renderText={(value) => (
-          <>
-            <p>
-              subtotal ({basket.length} items): <strong>${value}</strong>
-            </p>
-            <small className="subtotal_gift">
-              <input type="checkbox" />
-              This order contains a gift
-            </small>
-          </>
-        )}
-        decimalScale={2}
-        value={getBasketTotal(basket)}
-        displayType="text"
-        thousandSeparator={true}
-      />
+      <p>
+        Subtotal ({basket.length} items):{" "}
+        <strong>
+          $
+          <CurrencyInput
+            value={getBasketTotal(basket)}
+            decimalScale={2}
+            fixedDecimalScale={true}
+            prefix="$"
+            decimalSeparator="."
+            groupSeparator=","
+            disabled
+          />
+        </strong>
+      </p>
+      <small className="subtotal_gift">
+        <input type="checkbox" />
+        This order contains a gift
+      </small>
       <button className="subtotal_button">Proceed to Checkout</button>
     </div>
   );
